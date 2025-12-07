05f066731f12749c9ed41c65e5b50c30f5c4bd07
z125503-KAIFANG-LEE <z125503@shibaura-it.ac.jp>
2025-12-07 23:59:44 +0900

    hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int a[2][2];
+	int i, j, deter;
+	printf("Enter the ellements in the array:");
+	for(i = 0; i < 2; i++)
+	{
+		for(j=0; j<2; j++)
+		{
+			printf("enter the elements in [%d][%d]:", i,j);
+			scanf("%d", &a[i][j]);
+		}
+	}
+	printf("the result of your input array:\n");
+	for ( i=0; i<3; i++)
+	{
+		for(j=0; j<2; j++)
+		{
+			printf("%d\t", a[i][j]);
+		}
+	printf("\n");
+	}
+	deter = a[0][0] * a[1][1] - a[0][1]*a[1][0];
+return 0;
+}
+
+
 
