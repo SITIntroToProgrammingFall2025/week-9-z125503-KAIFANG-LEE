5e295c95c66e319de1e7f7a0954eae284bc9baae
z125503-KAIFANG-LEE <z125503@shibaura-it.ac.jp>
2025-12-08 00:13:58 +0900

    hw9-2.c

--- hw9-2.c
+++ hw9-2.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][3];
+	int  b[3][3];
+	int i, j;
+	for(i = 0; i < 3; i++)
+		{
+			for(j=0; j<3; j++)
+			{
+				scanf("%d", &a[i][j]);
+			}
+		}
+	a[0][0] =b[0][2];
+	a[0][2] = b[0][0];
+	a[1][0] = b[1][2];
+	a[1][2] = b[1][0];
+	a[2][0] = b[2][2];
+	a[2][2] = b[2][0];
+for ( i=0; i<3; i++)
+	{
+		printf("%d ", b[i][j]);
+	}
+	printf("\n");
+}
+return 0;
+}
+
 
