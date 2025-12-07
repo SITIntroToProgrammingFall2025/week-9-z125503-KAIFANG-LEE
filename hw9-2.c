01133e0d6a43b40bbdce98e7d02f83b2e8db4e6a
z125503-KAIFANG-LEE <z125503@shibaura-it.ac.jp>
2025-12-08 00:22:15 +0900

    hw9-2.c

--- hw9-2.c
+++ hw9-2.c
@@ -17,13 +17,24 @@ int main()
 	a[1][2] = b[1][0];
 	a[2][0] = b[2][2];
 	a[2][2] = b[2][0];
+printf("You entered\n");
 for ( i=0; i<3; i++)
 	{
-		printf("%d ", b[i][j]);
-	}
+		for(j=0; j<3; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
 	printf("\n");
+	}
+printf("Output\n");
+for ( i=0; i<3; i++)
+	{
+		for(j=0; j<3; j++)
+		{
+			printf("%d ", b[i][j]);
+		}
+	printf("\n");
+	}
+
 return 0;
 }
-
-
-
