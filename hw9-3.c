9926995e03bac85c03522369f7efe5667c5fce76
z125503-KAIFANG-LEE <z125503@shibaura-it.ac.jp>
2025-12-08 01:06:35 +0900

    hw9-3.c

--- hw9-3.c
+++ hw9-3.c
@@ -1,43 +1,52 @@
 #include <stdio.h>
-#include <math.h> // fabs
+#include <math.h>
 
 int main()
 {
     double a[2][3], b[3][2], c[2][2];
     int i, j;
 
-    // Input matrix A
     for(i = 0; i < 2; i++)
         for(j = 0; j < 3; j++)
             scanf("%lf", &a[i][j]);
 
-    // Input matrix B
     for(i = 0; i < 3; i++)
         for(j = 0; j < 2; j++)
             scanf("%lf", &b[i][j]);
 
-    // Multiply C = A × B
+    printf("The first matrix you entered is\n");
     for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+                printf("%d ", (int)a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("The second matrix you entered is\n");
+    for(i = 0; i < 3; i++)
     {
         for(j = 0; j < 2; j++)
         {
-            c[i][j] = a[i][0]*b[0][j] + a[i][1]*b[1][j] + a[i][2]*b[2][j];
+                printf("%d ", (int)b[i][j]);
         }
+        printf("\n");
     }
 
-    // Print result
+    for(i = 0; i < 2; i++)
+        for(j = 0; j < 2; j++)
+            c[i][j] = a[i][0]*b[0][j] + a[i][1]*b[1][j] + a[i][2]*b[2][j];
+
     printf("The multiplication product of matrix A and matrix B:\n");
     for(i = 0; i < 2; i++)
     {
         for(j = 0; j < 2; j++)
         {
-            // 判斷是否整數
             if (fabs(c[i][j] - (int)c[i][j]) < 1e-6)
-                printf("%d", (int)c[i][j]);
+                printf("%d ", (int)c[i][j]);
             else
-                printf("%.1lf", c[i][j]);
-
-            if (j != 1) printf(" "); // 空格
+                printf("%.1lf ", c[i][j]);
         }
         printf("\n");
     }
