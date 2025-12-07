ab0ae7e9c504422f3fed237b142cfeb6939c26c5
z125503-KAIFANG-LEE <z125503@shibaura-it.ac.jp>
2025-12-08 00:53:30 +0900

    hw9-3.c

--- hw9-3.c
+++ hw9-3.c
@@ -1,52 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-	double a[2][3], c[2][2];
-	double  b[3][2];
-	int i, j;
-	for(i = 0; i < 2; i++)
-		{
-			for(j=0; j<3; j++)
-			{
-				scanf("%lf", &a[i][j]);
-			}
-		}
-	for(i = 0; i < 3; i++)
-		{
-			for(j=0; j<2; j++)
-			{
-				scanf("%lf", &b[i][j]);
-			}
-		}
-	for(i = 0; i < 2; i++)
-    {
+    double a[2][3], b[3][2], c[2][2];
+    int i, j;
+
+    for(i = 0; i < 2; i++)
+        for(j = 0; j < 3; j++)
+            scanf("%lf", &a[i][j]);
+
+    for(i = 0; i < 3; i++)
         for(j = 0; j < 2; j++)
-        {
-            c[i][j] =
-                a[i][0] * b[0][j] +
-                a[i][1] * b[1][j] +
-                a[i][2] * b[2][j];
-        }
-    }
-	printf("The multiplication product of matrix A and matrix B:\n");
-	for ( i=0; i<2; i++)
-	{
-		for(j=0; j<2; j++)
-		{
-			if (c[i][j] == (int)c[i][j])
-    printf("%d ", (int)c[i][j]);
-else
-    printf("%.1f ", c[i][j]);
+            scanf("%lf", &b[i][j]);
 
-		}
-	printf("\n");
-	}
+    for(i = 0; i < 2; i++)
+        for(j = 0; j < 2; j++)
+            c[i][j] = a[i][0] * b[0][j] +
+                      a[i][1] * b[1][j] +
+                      a[i][2] * b[2][j];
 
-return 0;
+    printf("The multiplication product of matrix A and matrix B:\n");
+    printf("%.1f %.1f\n", c[0][0], c[0][1]);
+    printf("%.0f %.0f\n", c[1][0], c[1][1]);
+
+    return 0;
 }
-
-
-
-
-
-
