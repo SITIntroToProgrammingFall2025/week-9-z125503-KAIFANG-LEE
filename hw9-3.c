18a11b12dfde0ed95d448acf7b3c39a761d07d2c
z125503-KAIFANG-LEE <z125503@shibaura-it.ac.jp>
2025-12-08 14:19:59 +0900

    hw9-3.c

--- hw9-3.c
+++ hw9-3.c
@@ -1,55 +1,53 @@
-#include <stdio.h>
-#include <math.h>
-
+#include<stdio.h>
 int main()
 {
-    double a[2][3], b[3][2], c[2][2];
-    int i, j;
-
-    for(i = 0; i < 2; i++)
-        for(j = 0; j < 3; j++)
-            scanf("%lf", &a[i][j]);
-
-    for(i = 0; i < 3; i++)
-        for(j = 0; j < 2; j++)
-            scanf("%lf", &b[i][j]);
-
-    printf("The first matrix you entered is\n");
-    for(i = 0; i < 2; i++)
-    {
-        for(j = 0; j < 3; j++)
-        {
-                printf("%d ", (int)a[i][j]);
+	double a[2][3], c[2][2];
+	double  b[3][2];
+	int i, j;
+	for(i = 0; i < 2; i++)
+		{
+			for(j=0; j<3; j++)
+			{
+				scanf("%lf", &a[i][j]);
+			}
+		}
+	for(i = 0; i < 3; i++)
+		{
+			for(j=0; j<2; j++)
+			{
+				scanf("%lf", &b[i][j]);
+			}
+		}
+	for(i = 0; i < 2; i++)
+        	for(j = 0; j < 2; j++)
+            		c[i][j] = a[i][0]*b[0][j] + a[i][1]*b[1][j] + a[i][2]*b[2][j];
+	printf("The first matrix you entered is\n");
+   	for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printNumber(A[i][j]);
+            printf(" ");
         }
         printf("\n");
     }
-
-    printf("The second matrix you entered is\n");
-    for(i = 0; i < 3; i++)
-    {
-        for(j = 0; j < 2; j++)
-        {
-                printf("%d ", (int)b[i][j]);
+printf("The second matrix you entered is\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            printNumber(B[i][j]);
+            printf(" ");
         }
         printf("\n");
     }
+	for ( i=0; i<2; i++)
+	{
+		for(j=0; j<2; j++)
+		{
+			if (C[i][j] == (int)C[i][j])
+    printf("%d ", (int)C[i][j]);
+else
+    printf("%.1f ", C[i][j]);
+		}
+	printf("\n");
+	}
 
-    for(i = 0; i < 2; i++)
-        for(j = 0; j < 2; j++)
-            c[i][j] = a[i][0]*b[0][j] + a[i][1]*b[1][j] + a[i][2]*b[2][j];
-
-    printf("The multiplication product of matrix A and matrix B:\n");
-    for(i = 0; i < 2; i++)
-    {
-        for(j = 0; j < 2; j++)
-        {
-            if (fabs(c[i][j] - (int)c[i][j]) < 1e-6)
-                printf("%d ", (int)c[i][j]);
-            else
-                printf("%.1lf ", c[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
+return 0;
 }
