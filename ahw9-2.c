e497fee868626864209f8cfbaaec8a01f4a47500
z125503-KAIFANG-LEE <z125503@shibaura-it.ac.jp>
2025-12-09 18:17:16 +0900

    ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int a[5];
+    int i, j;
+
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    // bubble sort without temp
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4 - i; j++) {
+            if (a[j] > a[j + 1]) {
+                a[j]     = a[j] + a[j + 1];
+                a[j + 1] = a[j] - a[j + 1];
+                a[j]     = a[j] - a[j + 1];
+            }
+        }
+    }
+
+    for (i = 0; i < 5; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
+
 
