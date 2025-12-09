46984c5a80726d4ffffe1c374c2d345ef3a5e9c9
z125503-KAIFANG-LEE <z125503@shibaura-it.ac.jp>
2025-12-09 18:08:58 +0900

    ahw9-1.c

--- ahw9-1.c
+++ ahw9-1.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int r, g, b;
+
+    scanf("%d %d %d", &r, &g, &b);
+
+    
+    int d_red   = (r - 255)*(r - 255) + g*g + b*b;
+    int d_green = r*r + (g - 255)*(g - 255) + b*b;
+    int d_blue  = r*r + g*g + (b - 255)*(b - 255);
+
+    int min = d_red;
+    char *nearest = "Red";
+
+    if (d_green < min) {
+        min = d_green;
+        nearest = "Green";
+    }
+    if (d_blue < min) {
+        min = d_blue;
+        nearest = "Blue";
+    }
+
+    printf("Nearest color = %s\n", nearest);
+    return 0;
+}
+
 
